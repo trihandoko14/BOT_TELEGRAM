996b0d48f5cebc2f72df6998814c8fe10e9d48ac
trihandoko14 <95297180+trihandoko14@users.noreply.github.com>
2021-12-31 16:19:35 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/ercooss/UAS_SC/main/public/dnn_model/model.json';
+        const path = 'https://raw.githubusercontent.com/trihandoko14/BOT_TELEGRAM/main/public/dnn_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
